517a849ccc07b00af5b0ed2b27768c9ed2b7b301
Adil Rahmani <didilusse@gmail.com>
2025-05-29 18:00:21 -0400

    Initial Commit

---
+++ Govee Sync/Govee_SyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  Govee_SyncApp.swift
+//  Govee Sync
+//
+//  Created by Adil Rahmani on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Govee_SyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
